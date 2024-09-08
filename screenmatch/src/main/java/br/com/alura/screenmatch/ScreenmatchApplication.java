7ea127affb8cf6ff1eeb353c47466bf48490b2bc
Matheus Carneiro <matheusalc017@gmail.com>
2024-09-08 19:26:33 -0300

    Iniciando aplicação Spring

--- screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -14,6 +14,6 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		System.out.println("Primeiro projeto em Java com Spring");
 	}
 }
